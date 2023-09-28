677f12bfd8aba32c5233ec442f62ab631324df83
jin-jae <jinjae.dev@gmail.com>
2023-09-28 16:49:20 +0900

    feat: docker compose (orchestration)

--- main.sh
+++ main.sh
@@ -1,1 +1,5 @@
-docker-compose up --build
+git clone https://github.com/BO-DA/boda_AI boda-ai
+
+docker compose up --build -d
+
+docker exec -it boda-ai-server /bin/bash /home/boda/boda-ai-server.sh
